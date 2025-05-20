6872213932dbb315df1e90b18b31f4825cf97b70
Kutukobra <nadzhiff@gmail.com>
2025-05-20 23:26:26 +0700

    Centered Hamburger

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -85,11 +85,11 @@ return (
                     })
                     
                 }
-                {token && <button onClick={handleLogout} className="hover:underline hover:text-[#89b4fa] transition duration-300">Logout</button>}
+                {token && <button onClick={handleLogout} className="hover:underline hover:text-[#89b4fa] transition duration-300 ">Logout</button>}
             </div>
             
-            <button className='block sm:hidden h-8 w-8 align-middle text-center' onClick={() => {setOpen(!open)}}>
-                <svg className="fill-white h-6 w-6" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 24.75 24.75" xml:space="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <path d="M0,3.875c0-1.104,0.896-2,2-2h20.75c1.104,0,2,0.896,2,2s-0.896,2-2,2H2C0.896,5.875,0,4.979,0,3.875z M22.75,10.375H2 c-1.104,0-2,0.896-2,2c0,1.104,0.896,2,2,2h20.75c1.104,0,2-0.896,2-2C24.75,11.271,23.855,10.375,22.75,10.375z M22.75,18.875H2 c-1.104,0-2,0.896-2,2s0.896,2,2,2h20.75c1.104,0,2-0.896,2-2S23.855,18.875,22.75,18.875z"></path> </g> </g></svg>
+            <button className='sm:hidden h-8 w-8 align-middle text-center block' onClick={() => {setOpen(!open)}}>
+                <svg className="fill-white h-6 w-6 -translate-x-3 -translate-y-1" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 24.75 24.75" xml:space="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <path d="M0,3.875c0-1.104,0.896-2,2-2h20.75c1.104,0,2,0.896,2,2s-0.896,2-2,2H2C0.896,5.875,0,4.979,0,3.875z M22.75,10.375H2 c-1.104,0-2,0.896-2,2c0,1.104,0.896,2,2,2h20.75c1.104,0,2-0.896,2-2C24.75,11.271,23.855,10.375,22.75,10.375z M22.75,18.875H2 c-1.104,0-2,0.896-2,2s0.896,2,2,2h20.75c1.104,0,2-0.896,2-2S23.855,18.875,22.75,18.875z"></path> </g> </g></svg>
             </button>
         </div>
     </nav>
